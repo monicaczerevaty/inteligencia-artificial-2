aabba7d4b3efc8e9c4a5c363c8ad29c2b98e6e61
monicaczerevaty <111015067+monicaczerevaty@users.noreply.github.com>
2024-06-19 15:13:26 -0300

    1906

--- script.js
+++ script.js
@@ -6,18 +6,33 @@ const textoResultado = document.querySelector('.texto-resultado');
 const lista = [item1, item2]
 const perguntas = [
     {
-        enunciado: "Pergunta 1",
-        alternativas: ["Alternativa 1" , "Alternativa 2"],
+        enunciado: "Você decide o futuro da IA Assim que saiu da escola Gabriel se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, o chat também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento de Gabriel?",
+        alternativas: ["Isso é assustador" , "Isso é maravilhoso!"],
     }.
     {
-        enunciado: "Pergunta 2",
-        alternativas: ["Alternativa 1" , "Alternativa 2"],
+        enunciado: "Com a descoberta desta tecnologia uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre IA. No fim de uma aula ela pede que Gabriel escreva um trabalho sobre o uso de tecnologia em sala de aula. Qual atitude Gabriel toma?",
+        alternativas: ["Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento." , "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema."],
     }.
     {
         enunciado: "Pergunta 3",
         alternativas: ["Alternativa 1" , "Alternativa 2"],
     }.
 ];
+let atual = 0;
+let perguntaAtual;
+function mostraPergunta(){
+    perguntaAtual = perguntas [atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+function mostraAlternativas(){
+    for (const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+mostraPergunta();
 const lapis = {
     tamanho: 20,
     tipo: 'HB',
