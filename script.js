be88e274df68e4e4ad297f5fb13aaffbd152eede
monicaczerevaty <111015067+monicaczerevaty@users.noreply.github.com>
2024-08-01 07:55:13 -0300

    01082024

--- script.js
+++ script.js
@@ -63,8 +63,13 @@ let perguntaAtual;
 let historiaFinal = "";
 
 function mostraPergunta(){
+    if (atual >= perguntas.length){
+mostraResultado();
+return;
+    }
     perguntaAtual = perguntas [atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
 function mostraAlternativas(){
@@ -75,13 +80,18 @@ function mostraAlternativas(){
             respostaSelecionada(alternativa)); 
             function respostaSelecionada (opcaoSelecionada){
                 const afirmacoes = opcaoSelecionada.afirmação;
-                historiaFinal = afirmacoes;
+                historiaFinal += afirmacoes+"";
                 atual++;
                 mostraPergunta();
             }
         );
         caixaAlternativas.appendChild(botaoAlternativas);
     }
+}
+function mostraResultado(){
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
 }
 mostraPergunta();
 
