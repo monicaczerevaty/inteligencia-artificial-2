118bbab1975b25f6292c01ab7a63f0aa58882a97
monicaczerevaty <111015067+monicaczerevaty@users.noreply.github.com>
2024-09-05 09:01:41 -0300

    0509

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ import {perguntas} from "./perguntas.js";
 const caixaPrincipal = document.querySelector('.caixa-principal');
 const caixaPerguntas = document.querySelector('.caixa-perguntas');
 const caixaAlternativas = document.querySelector('.caixa-alternativas');
-const caixaResultado = document.querySelector('.caixa-resultado');
+const caixaResultado = document.querySelector('.caixa-resultados');
 const textoResultado = document.querySelector('.texto-resultado');
 const botaoJogarNovamente = document.querySelector(".novamente-bnt");
 
