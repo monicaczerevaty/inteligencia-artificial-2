ac50c1b577727f5207d1703fc7be34c30f53efdb
monicaczerevaty <111015067+monicaczerevaty@users.noreply.github.com>
2024-09-26 08:19:45 -0300

    2609

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,6 +1,6 @@
 export const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: " Vocẽ acredita que o bullying é mais comun nas escolas ou em mais ambientes ",
         alternativas: [
             {
                 texto: "Isso é assustador!",
--- js/script.js
+++ js/script.js
@@ -1,15 +1,29 @@
 import { aleatorio, nome } from './aleatorio.js';
 import { perguntas } from './perguntas.js';
 
+const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultados = document.querySelector(".caixa-resultados");
 const textoResultado = document.querySelector(".texto-resultado");
 const botaoJogarNovamente = document.querySelector(".novamente-btn");
+const botaoIniciar = document.querySelector(".iniciar-bnt");
+const telaInicial = document.querySelector(".tela-principal");
 
 let atual = 0;
 let perguntaAtual;
 let historiaFinal = "";
+
+botaoIniciar.addEventListener('click',iniciaJogo);
+function iniciaJogo () {
+    atual = 0;
+    historiaFinal = "";
+    telaInicial.style.display = 'none';
+    caixaPerguntas.classList.remove("mostrar");
+    caixaAlternativas.classList.remove("mostrar");
+    caixaResultados.classList.remove("mostrar");
+    mostraPergunta();
+}
 
 function mostraPergunta() {
     if (atual >= perguntas.length) {
@@ -65,4 +79,3 @@ function substituiNome() {
 }
 
 substituiNome();
-mostraPergunta();
